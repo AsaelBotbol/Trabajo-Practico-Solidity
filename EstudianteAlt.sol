e1381b1298936cf4f04a5a4756837f7da7004ff8
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-06-15 15:34:43 -0300

    Update EstudianteAlt.sol

--- EstudianteAlt.sol
+++ EstudianteAlt.sol
@@ -9,3 +9,20 @@ contract Estudiante{
     mapping(uint8 => mapping(string => uint8)) private _notas_materias;
     string[] private _materias;
     mapping (address => bool) _permisos;
+    
+    event SetNota(address docente, string materia, uint8 bimestre, uint8 nota);
+
+    constructor(string memory nombre_, string memory apellido_, string memory curso_){
+        _nombre = nombre_;
+        _apellido = apellido_;
+        _curso = curso_;
+        _docente = msg.sender;
+    }
+
+function apellido() public view returns(string memory){
+        return _apellido;
+    }
+
+    function nombre_completo() public view returns(string memory){
+        return string(bytes.concat(bytes(_nombre)," ", bytes(_apellido)));
+    }
