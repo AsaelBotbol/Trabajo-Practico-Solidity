7cd51ee1c3eb1e03bc6d86cb3b9fa268587ef62b
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-06-15 15:31:10 -0300

    Add files via upload

---
+++ EstudianteAlt.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+contract Estudiante{
+    string private _nombre;
+    string private _apellido;
+    string private _curso;
+    address private _docente;
+    mapping(uint8 => mapping(string => uint8)) private _notas_materias;
+    string[] private _materias;
+    mapping (address => bool) _permisos;
