14e18757128f2e342a36668bbfcd32034339341f
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-06-15 10:22:53 -0300

    Declaracion

---
+++ Estudiante.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+contract Estudiante{
+    string private _nombre;
+    string private _apellido;
+    string private _curso;
+    address private _docente;
+    mapping (string => uint8) private _notas_materias;
+    string[] private _materias;
